976b48339945d2d8a8261156e38149af08272aa7
Chance Cardona <ccardona@mymail.mines.edu>
2020-01-24 01:23:33 -0700

    Rough Outline

---
+++ find_route.py
@@ -0,0 +1,25 @@
+#import libs
+
+#create data array from input file
+
+#form tree
+
+#uninformed search
+#using DFS. need LIFO queue.
+
+
+#frontier.append(start)
+#while frontier.len > 0
+    #cur = pop.frontier
+    #if cur == goal
+        #return
+    #if cur not in explored
+        #explored.append(cur)
+    #frontier.append(cur.children[i])
+    #i++
+
+
+
+
+
+#output simplified results
---
+++ readme.txt
@@ -0,0 +1,5 @@
+1. Chance Cardona, ccardona@mymail.mines.edu, 10728053
+2. Python 3
+3. Windows 10 and ArchLinux
+4. Not sure yet
+5. python3 find_route.py input_filename from_city to_city
