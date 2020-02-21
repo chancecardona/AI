120df73edc9ef8f86f6018ba33c0e68781a73913
Chance Cardona <ccardona@mymail.mines.edu>
2020-02-20 23:57:55 -0700

    Cleaned Minimax Code. Final Minimax before Alpha Beta Pruning Work

--- project2/minimax.py
+++ project2/minimax.py
@@ -2,12 +2,10 @@
 
 # Adversarial Search using Minimax Algorithm.
 # Created by Chance Cardona 2/19/2020
+
 from math import inf
-from copy import deepcopy
+
 # returns [move, score] here move is column.
-# state is currentGame.gameBoard
-# player is currentGame.currentTurn
-# depth is int value input
 def minimax(currentGame, player, depth):
     #Game has points, so not 0 sum.
     if player == 1:     #1 is MAX   -1 is MIN
@@ -16,8 +14,8 @@ def minimax(currentGame, player, depth):
         best = [-1, inf]
 
     if depth == 0 or currentGame.pieceCount == 42:
-        #print("TERMINAL NODE")
         currentGame.countScore()
+        #TODO find a better Evaluation Function than current Score
         if currentGame.currentTurn == 1:
             score = currentGame.player1Score
         else:
@@ -30,25 +28,14 @@ def minimax(currentGame, player, depth):
     notFull = (col for col in range(len(state[0])) if sumCol[col] < len(state))
     for col in notFull:
         prevGame = currentGame.fullCopy()
-        #print("B4", currentGame.pieceCount, depth)
-        currentGame.playPiece(col) #Execute move
-        #if depth == 10:
-        #print("Current Game at Depth and col", depth, col)
-        #currentGame.printGameBoard()
-        #print("Prev Game:")
-        #prevGame.printGameBoard()
-        [move, score] = minimax(currentGame, -player, depth - 1) #Recur
-        currentGame = prevGame #Undo move
-    #    print("After", currentGame.pieceCount, depth)
+        currentGame.playPiece(col)                                  #Execute move
+        [move, score] = minimax(currentGame, -player, depth - 1)    #Recur
+        currentGame = prevGame                                      #Undo move
         if player == 1:
             if score > best[1]:
                 best = [col, score]
-                #print("New Best Score! Col", col, "Depth", depth)
-                #currentGame.printGameBoard()
         else:
             if score < best[1]:
                 best = [col, score]
 
-    #print("Minimax is Returning!!! Depth", depth)
-    #currentGame.printGameBoard()
     return best
